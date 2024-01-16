6140fc8dd71757330f859e2b0e797e0b172bb9ab
Sippbox <coltonthibert2@hotmail.co.uk>
2024-01-15 21:45:08 -0500

    Potential fix for ban messages not sending

--- src/main/java/com/sippbox/bot/commands/commands/ScamBanCommand.java
+++ src/main/java/com/sippbox/bot/commands/commands/ScamBanCommand.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
@@ -19,7 +20,7 @@ public class ScamBanCommand extends SlashCommand {
     public String name() { return "scam"; }
 
     @Override
-    public String description() { return "Bans a user for scamming, confirms with a button, sends appeal info to banned user."; }
+    public String description() { return "Bans a user for scamming, sends appeal info to banned user."; }
 
     @Override
     public Permission[] neededPermissions() { return new Permission[]{Permission.USE_APPLICATION_COMMANDS, Permission.BAN_MEMBERS}; }
@@ -42,18 +43,11 @@ public class ScamBanCommand extends SlashCommand {
             return;
         }
 
-        AtomicBoolean messageSent = new AtomicBoolean(false);
-        member.getUser().openPrivateChannel().queue(channel -> {
-            channel.sendMessageEmbeds(createUserEmbed()).queue(success -> {
-                messageSent.set(true);
-                sendModEmbed(info, member, messageSent.get());
-            }, failure -> {
-                messageSent.set(false);
-                sendModEmbed(info, member, messageSent.get());
-            });
-        });
-
-        info.event().getGuild().ban(member, 7, TimeUnit.DAYS).queue();
+        member.getUser().openPrivateChannel()
+                .flatMap(channel -> channel.sendMessageEmbeds(createUserEmbed()))
+                .mapToResult()
+                .flatMap(x -> info.event().getGuild().ban(member, 7, TimeUnit.DAYS).reason("[Sippbot] Scamming"))
+                .queue(success -> sendModEmbed(info, member, true), failure -> sendModEmbed(info, member, false));
     }
 
     private void sendModEmbed(SlashCommandRecord info, Member member, boolean messageSent) {
@@ -63,7 +57,7 @@ public class ScamBanCommand extends SlashCommand {
     private MessageEmbed createUserEmbed() {
         return new EmbedBuilder()
                 .setTitle("Ban Notification")
-                .setDescription("You have been banned from Sipp's Avatar Box because malicious/scamming activity was detected from your account. If you are not a scammer and we made a mistake, please fill out the appeal form below. If you *are* a scammer, please stop wasting our time and yours. **You know who you are.**")
+                .setDescription("You have been banned from Sipp's Avatar Box because malicious/scamming activity was detected from your account. If you are not a scammer and we made a mistake, please fill out the appeal form below.")
                 .addField("Appeal Form", "https://dyno.gg/form/e887a05c", false)
                 .setThumbnail("https://cdn.discordapp.com/icons/873806377183752212/a_16f71e81793a68f20ebce0ad5145b11b.gif?size=128")
                 .setColor(Color.RED)
@@ -74,12 +68,12 @@ public class ScamBanCommand extends SlashCommand {
         return new EmbedBuilder()
                 .setColor(Color.RED)
                 .setTitle("Scammer Banned")
-                .setDescription(member.getUser().getAsMention() + " has been banned for scamming!")
-                .addField("Account Age", "<t:" + member.getUser().getTimeCreated().toInstant().getEpochSecond() + ":f>", true)
+                .setDescription(member.getAsMention() + " has been banned for scamming!")
+                .addField("Account Age", "<t:" + member.getTimeCreated().toInstant().getEpochSecond() + ":f>", true)
                 .addField("Joined Server", "<t:" + member.getTimeJoined().toInstant().getEpochSecond() + ":f>", false)
                 .addField("Appeal Message Sent", messageSent ? "True" : "False", false)
-                .setThumbnail(member.getUser().getAvatarUrl())
-                .setFooter("User ID: " + member.getUser().getId())
+                .setThumbnail(member.getAvatarUrl())
+                .setFooter("User ID: " + member.getId())
                 .build();
     }
 }
